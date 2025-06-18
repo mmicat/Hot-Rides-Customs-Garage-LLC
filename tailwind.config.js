d3c045be538d6d9621d2cd9f1c0638826659a043
mmicat <mi6491@g.rit.edu>
2025-06-18 17:12:41 +0400

    landing current ver

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	prefix: 'tw-',
+	important: false,
+	content: [
+		"**/*.{html, jsx, js}",
+		"**/*.js",
+		"**/*.html",
+	],
+	darkMode: 'class',
+	theme: {
+		extend: {
+			colors: {
+				primary: "#d80808",
+				secondary: "#980808", 
+			}
+		},
+	},
+	plugins: [],
+}
+
